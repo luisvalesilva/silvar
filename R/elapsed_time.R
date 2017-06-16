4e85846e4726da6c02844c2b91aed68a714433b1
luisvalesilva <luisvalesilva@gmail.com>
2017-06-16 18:18:07 -0400

    Refactored function

--- R/elapsed_time.R
+++ R/elapsed_time.R
@@ -1,23 +1,31 @@
-#' Returns elapsed time
+#' Return elapsed time
 #'
-#' Given a time stamp generated with \code{proc.time}, returns elapsed time in sec.,
-#' min. or hrs, as appropriate.
-#' @param t0 Output of \code{proc.time}, either all elements or just element "elapsed".
-#' No default.
-#' @return Elapsed time as a \code{string}.
+#' Given a start and end time in seconds, returns elapsed time in sec., min. or
+#' hrs, as appropriate.
+#' @param t0 Numeric start time representing seconds. No default.
+#' @param t1 Numeric end time representing continuous second count from
+#' \code{t0}. No default.
+#' @return \code{String} containing elapsed time.
 #' @examples
-#' start <- proc.time()
-#' elapsed_time(t0=start)
-#' 
+#' \dontrun{
 #' start <- proc.time()[3]
-#' elapsed_time(t0=start)
+#' end <- proc.time()[3]
+#' elapsed_time(t0=start, t1=end)
+#' 
+#' start <- proc.time()
+#' elapsed_time(start[3], proc.time()[3])
+#' }
 #' @export
 
-elapsed_time <- function(t0){
-  # Are all elements returned by proc.time included?
-  if (length(t0) > 1) t0 <- t0[3]
+elapsed_time <- function(t0, t1){
+  
+  # IO checks
+  if (!(is(t0, "numeric") && is(t1, "numeric"))) {
+    stop('"t0" and "t1" must be numeric')
+  }
+  
   # Calculate elapsed time
-  elapsed_time <- proc.time()[3] - t0
+  elapsed_time <- t1 - t0
   
   # Convert to appropriate unit
   if (elapsed_time < 60) {
@@ -25,7 +33,7 @@ elapsed_time <- function(t0){
   } else if (elapsed_time >= 60 & elapsed_time < 3600) {
     elapsed_time <- paste0(round(elapsed_time / 60, 1), " min.")
   } else {
-    elapsed_time <- paste0(round(elapsed_time / 60 / 60, 1), " h.")
+    elapsed_time <- paste0(round(elapsed_time / 60 / 60, 2), " hrs.")
   }
   return(elapsed_time)
 }
